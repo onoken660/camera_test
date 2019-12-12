79a2eb6b09ca45342b8309c36dc6389cffa69cc5
onoken660 <58803475+onoken660@users.noreply.github.com>
2019-12-12 15:44:19 +0900

    Create camera.js

---
+++ camera.js
@@ -0,0 +1,25 @@
+const medias = {
+  audio : false,
+  video : true
+};
+function init_video() {
+  if (navigator.mediaDevices) {
+    var promise = navigator.mediaDevices.getUserMedia(medias);
+    promise.then(successCallback).catch(errorCallback);
+  }
+}
+
+if(window.addEventListener) {
+  window.addEventListener('load', init_video);
+} else if(window.attachEvent) {
+  window.attachEvent('load', init_video);
+}
+
+function successCallback(stream) {
+  var video = document.getElementById('video');
+  video.srcObject = stream;
+};
+
+function errorCallback(err) {
+  alert(err);
+};
