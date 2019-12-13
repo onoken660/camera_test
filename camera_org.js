a42fbf0b3f588c570fe3acc1425bb181fc583c9a
onoken660 <58803475+onoken660@users.noreply.github.com>
2019-12-13 11:45:40 +0900

    Create camera_org.js

---
+++ camera_org.js
@@ -0,0 +1,17 @@
+const medias = {
+  audio : false,
+  video : true
+};
+const video = document.getElementById("video");
+const promise = navigator.mediaDevices.getUserMedia(medias);
+
+promise.then(successCallback)
+       .then(errorCallback);
+
+function successCallback(stream) {
+  video.srcObject = stream;
+};
+
+function errorCallback(err) {
+  alert(err);
+};
