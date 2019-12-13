4abb72dab1a4dbd4e44f40487a2529aae97f6ad7
onoken660 <58803475+onoken660@users.noreply.github.com>
2019-12-13 13:10:13 +0900

    Add files via upload

---
+++ test001.files/test_cam.files/test_cam_20$7689524.js
@@ -0,0 +1,53 @@
+function popup_show() {
+	document.getElementById('layer').style.display = 'block';
+	document.getElementById('popup').style.display = 'block';
+}
+function popup_hide() {
+	document.getElementById('layer').style.display = 'none';
+	document.getElementById('popup').style.display = 'none';
+}
+const medias = {
+	audio: false,
+	video: {
+		facingMode: {exact:"environment"}	
+	}
+};
+function init_video() {
+	video.setAttribute("playsinline", true);
+	if (navigator.mediaDevices) {
+		var promise = navigator.mediaDevices.getUserMedia(medias);
+		promise.then(successCallback).catch(errorCallback);
+	}
+	else {
+		alert('0');
+	}
+}
+if(window.addEventListener) {
+  window.addEventListener('load', init_video);
+} else if(window.attachEvent) {
+  window.attachEvent('load', init_video);
+}
+function successCallback(stream) {
+	video.srcObject = stream;
+}
+function errorCallback(err) {
+	alert(err);
+}
+function take_pict() {
+	var camera = document.getElementById('video');
+	var paint_view = document.getElementById('img_file_table');
+	paint_view.style.display = 'block';
+	var draw_view = document.getElementById('img_file_draw');
+	draw_view.style.width = '450px';
+	draw_view.style.height = '350px';
+	var canvas = document.getElementById('img_file_canvas');
+	canvas.width = 400;
+	canvas.height = 300;
+	var ctx = canvas.getContext('2d');
+	try {
+		ctx.drawImage(camera, 0, 0, 400, 300);
+	} catch(error) {
+		alert(error);
+	}
+	popup_hide();
+}
